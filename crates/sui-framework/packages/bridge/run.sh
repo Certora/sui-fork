d77b310ffc8b58fcb8b76ef60e596070289895ab
Eric Eilebrecht <eric@certora.com>
2025-06-25 11:03:50 -0700

    WIP

--- crates/sui-framework/packages/bridge/run.sh
+++ crates/sui-framework/packages/bridge/run.sh
@@ -1,3 +1,3 @@
 set -e
 sui move build --test
-java -Dlevel.setup.helpers=warn -jar $CERTORA/emv.jar -treeViewReportUpdateInterval 0 -movePath build/Bridge/bytecode_modules/ -b 2 -assumeUnwindCond
+java -Dlevel.setup.helpers=warn -jar $CERTORA/emv.jar -treeViewReportUpdateInterval 0 -movePath build -b 2 -assumeUnwindCond
