230dada9cc31be0d35948024b2d2b61cf686c62b
Eric Eilebrecht <eric@certora.com>
2025-09-03 13:18:46 -0700

    WIP

--- bridge/move/certora/spec/runCloud.sh
+++ bridge/move/certora/spec/runCloud.sh
@@ -1,3 +1,3 @@
 set -e
 sui move build --test
-certoraSuiProver.py Bridge.conf --server staging --prover_version eric/parametric --prover_args "-calltraceFreeOpt true"
+certoraSuiProver.py Bridge.conf --server staging --prover_version eric/opt --prover_args "-calltraceFreeOpt true"
--- bridge/move/certora/spec/runLocal.sh
+++ bridge/move/certora/spec/runLocal.sh
@@ -1,5 +1,5 @@
 set -e
 sui move build --test
-certoraSuiProver.py Bridge.conf --prover_args "-calltraceFreeOpt true -includeMoveRules only_claiming_mints_tokens"
+certoraSuiProver.py Bridge.conf --prover_args "-calltraceFreeOpt true"
 
-# -includeMoveTargetNames claim_token
+# 
